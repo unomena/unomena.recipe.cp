1a33e1f4f7cfe3e5dcc8afc7f0f7e9705f4e94ed
Max Naude <max@unomena.com>
2011-07-30 16:47:43 +0200

    Bumped version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unomena.recipe.cp',
-    version='0.0.1',
+    version='0.0.2',
     description='Unomena linux cp buildout recipe',
     author='Unomena',
     author_email='dev@unomena.com',
